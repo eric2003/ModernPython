d2ec8afd55d666cf5f2857a3501d0523a118dff3
eric <fantasy_2003_@hotmail.com>
2022-06-17 00:37:36 +0800

    add quiver01

---
+++ codes/matplotlibcpp/quiver/01/main.cpp
@@ -0,0 +1,27 @@
+#define _USE_MATH_DEFINES // for C++
+#include <cmath>
+#include "matplotlibcppModified.h"
+#include <iostream>
+
+namespace plt = matplotlibcpp;
+
+int main() 
+{    
+    std::vector<std::vector<double>> x, y, z;
+    for (double i = -5; i <= 5;  i += 0.25) {
+        std::vector<double> x_row, y_row, z_row;
+        for (double j = -5; j <= 5; j += 0.25) {
+            x_row.push_back(i);
+            y_row.push_back(j);
+            z_row.push_back(::std::sin(::std::hypot(i, j)));
+        }
+        x.push_back(x_row);
+        y.push_back(y_row);
+        z.push_back(z_row);
+    }
+
+    plt::plot_surface(x, y, z);
+    plt::show();
+
+    return 0;
+}
